40b7cb667648f435a7edad324cf5e2001989363e
Emad Hassan Khan <emadkhanqai@gmail.com>
2023-08-28 23:40:41 +0500

    Add project files.

---
+++ Zepcom.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Zepcom.MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
